151b464f4b24775faafbc36e55ae4ce72d8f289d
Edward Printer <edwardprxnter@gmail.com>
2024-10-17 20:06:25 +0000

    display division not loading on pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,7 +144,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-// Add your division question here
+
 function displayDivisionQuestion(operand1, operand2) {
     operand1 = operand1 * operand2;
     document.getElementById("operand1").textContent = operand1;
